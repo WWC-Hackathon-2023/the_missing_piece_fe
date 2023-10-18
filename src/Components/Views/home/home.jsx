94979ad9139efd39645e4026b8c5a78b23aee047
Carmen Luna <lunacarmen22@gmail.com>
2023-10-18 08:09:51 -0500

    carmenlunap re-create home

---
+++ src/Components/Views/home/home.jsx
@@ -0,0 +1,44 @@
+import "./home.css";
+import { Link } from 'react-router-dom';
+import LogInButton from "../../commonComponents/logInButton/logInButton.jsx";
+import SignUpButton from "../../commonComponents/signUpButton/signUpButton.jsx";
+import logo_title from "../../../assets/logo_title.png";
+
+export default function Home() {
+
+    return (
+        <>
+            <header className="headerHome">
+                <SignUpButton />
+                <LogInButton />
+            </header>
+            
+            <div className="mainHome">
+                
+                <div className="welcomeMessage">
+                    <h1>Welcome To The Puzzle Community!</h1>
+                    <p>If you have a collection of puzzles and you're eager to swap
+                        them for new challenges, you're in the right spot. Join our community
+                        of fellow puzzle enthusiasts and dive into a world of delightful exchanges.
+                        Let's share the joy of solving puzzles together!
+                    </p>
+                    <Link to="/index">
+                        <button className="seeallPuzzlesButton">See All Puzzles</button>
+                    </Link>
+                </div>
+
+                <div className="ContainerLogo">
+                    <img src={logo_title} className="logo" alt="The Missing Piece" />
+                </div>
+                
+            </div>
+
+            <footer>
+                <h4>© 2023 The Missing Piece Team</h4>
+            </footer>
+        </>
+    )
+}
+
+
+
