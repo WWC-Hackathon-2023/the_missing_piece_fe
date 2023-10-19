6aa04588f81e3b4d59a5ff9c636a07c7602f3613
Natalia392 <ntorrejonvalencia@gmail.com>
2023-10-19 19:47:06 -0300

    natalia392 style add button to search for puzzles, put button on right side

--- src/Components/Views/dashboard/dashboard.jsx
+++ src/Components/Views/dashboard/dashboard.jsx
@@ -19,35 +19,40 @@ export default function Dashboard() {
         <NavigationBar />
       </header>
       <main className="main-profile">
-        <section className="profile-section">
-          <div className="profile-actions">
-            <img className="icon-person" src={iconPerson} alt="Profile Icon" />
-            <button className="add-puzzle-btn profile-btn" /*onClick={onEditProfileClick}*/>Add Puzzle</button>
-            <button className="update-profile-btn profile-btn" /*onClick={onAddPuzzleClick}*/>Update Profile</button>
+        <section className="profile-btn-section">
+          <div className="profile-div">
+            <div className="profile-actions">
+              <img className="icon-person" src={iconPerson} alt="Profile Icon" />
+              <button className="add-puzzle-btn profile-btn" /*onClick={onEditProfileClick}*/>Add Puzzle</button>
+              <button className="update-profile-btn profile-btn" /*onClick={onAddPuzzleClick}*/>Update Profile</button>
+            </div>
+            <div className="user-info">
+              <h2>Andrea Ramirez</h2> {/* Aquí sería con interpolación {user.name} */}
+              <div className="user-data-div">
+                <img className="icons-profile" src={zipIcon} alt="Zip Icon" />
+                <div className="user-data-text">
+                  <strong>Zip Code</strong> {/* {user.zipCode} */}
+                  <p>7100000</p>
+                </div>
+              </div>
+              <div className="user-data-div">
+                <img className="icons-profile" src={emailIcon} alt="Email Icon" />
+                <div className="user-data-text">
+                  <strong>Email</strong> {/* {user.email} */}
+                  <p>andrea@mail.org</p>
+                </div>
+              </div>
+              <div className="user-data-div">
+                <img className="icons-profile" src={phoneIcon} alt="Phone Icon" />
+                <div className="user-data-text">
+                  <strong>Phone</strong> {/* {user.phone} */}
+                  <p>+56 (123)4563789</p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="user-info">
-            <h2>Andrea Ramirez</h2> {/* Aquí sería con interpolación {user.name} */}
-            <div className="user-data-div">
-              <img className="icons-profile" src={zipIcon} alt="Zip Icon" />
-              <div className="user-data-text">
-                <strong>Zip Code</strong> {/* {user.zipCode} */}
-                <p>7100000</p>
-              </div>
-            </div>
-            <div className="user-data-div">
-              <img className="icons-profile" src={emailIcon} alt="Email Icon" />
-              <div className="user-data-text">
-                <strong>Email</strong> {/* {user.email} */}
-                <p>andrea@mail.org</p>
-              </div>
-            </div>
-            <div className="user-data-div">
-              <img className="icons-profile" src={phoneIcon} alt="Phone Icon" />
-              <div className="user-data-text">
-                <strong>Phone</strong> {/* {user.phone} */}
-                <p>+56 (123)4563789</p>
-              </div>
-            </div>
+          <div className="btn-div">
+            <button className="search-puzzles-btn">Search for Puzzles</button>
           </div>
         </section>
         <section className="requests-for-user requests"></section>
