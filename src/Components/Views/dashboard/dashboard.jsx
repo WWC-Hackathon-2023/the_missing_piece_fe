8b0134a26b44fb753fa1bd5b062107dcc5008f5e
Natalia392 <ntorrejonvalencia@gmail.com>
2023-10-19 09:51:49 -0300

    natalia392 style: adjust elements in navigation bar

--- src/Components/Views/dashboard/dashboard.jsx
+++ src/Components/Views/dashboard/dashboard.jsx
@@ -5,15 +5,12 @@ export default function Dashboard() {
   return (
     <>
       <header className="headerProfile">
-        <div className="containerButtons">
-          <NavigationBar />
-        </div>
+        <NavigationBar />
       </header>
       <h1>"Hello Profile"</h1>
       <footer>
         <h4>© 2023 The Missing Piece Team</h4>
       </footer>
-      
     </>
   );
 }
