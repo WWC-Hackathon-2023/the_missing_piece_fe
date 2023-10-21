c6a99c19dd3a815e8fb8de032a0859305e430e9c
Paola Andrea Ramirez <129551206+paolandre@users.noreply.github.com>
2023-10-20 22:39:58 -0500

    paolandre feat add some changes to the borrowerInfo page

--- src/Components/Views/borrowerInfo/borrowerInfo.jsx
+++ src/Components/Views/borrowerInfo/borrowerInfo.jsx
@@ -10,46 +10,37 @@ import NavigationBar from "../../commonComponents/navigationBar/navigationBar";
 export default function BorrowerInfo() {
     return (
         <>
-            <header className="headerProfile">
+            <div className="headerProfile">
                 <NavigationBar />
-            </header>
+            </div>
 
-            <div className='main-container'>
-                <div className="headerHome"></div>
-
-                <main className="main-profile">
-                    <section className="profile-btn-section">
-                        <div className="profile-div">
-                            <div className="profile-actions">
-                                <img className="icon-person" src={iconPerson} alt="Profile Icon" />
-                            </div>
-
-                            <div className="middle-side">
-                                <div className="user-info">
-                                    <h2>Andrea Ramirez</h2> {/* Use interpolation here, e.g., {user.name} */}
-
-                                    <UserData icon={zipIcon} label="Zip Code" data="7100000" />
-                                    <UserData icon={emailIcon} label="Email" data="andrea@mail.org" />
-                                    <UserData icon={phoneIcon} label="Phone" data="+56 (123)4563789" />
-                                </div>
-                                <Link to="/index">
-                                    <button className="return-dashboard-button">Return to dashboard</button>
-                                </Link>
-                                <div className='right-side'>
-                                    <h3>Now you can contact Bisrat by email or phone, whichever suits you best!</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-
-                    <footer className='foo-ter'>
-                        <h4>© 2023 The Missing Piece Team</h4>
-                    </footer>
-                </main>
+            <div className="container-borrower">
+                <div className="left-side">
+                    <div className="profile-actions">
+                        <img className="icon-person" src={iconPerson} alt="Profile Icon" />
+                    </div>
+                </div>
+                <div className="middle-side">
+                    <div className="user-info">
+                        <h2>Andrea Ramirez</h2> {/* Use interpolation here, e.g., {user.name} */}
+                        <UserData icon={zipIcon} label="Zip Code" data="7100000" />
+                        <UserData icon={emailIcon} label="Email" data="andrea@mail.org" />
+                        <UserData icon={phoneIcon} label="Phone" data="+56 (123)4563789" />
+                    </div>
+                    <Link to="/index">
+                        <button className="return-dashboard-button">Return to dashboard</button>
+                    </Link>
+                </div>
+                <div className='right-side'>
+                    <h3>Now you can contact Bisrat by email or phone, whichever suits you best!</h3>
+                </div>
+            </div>
+            <div className='foo-ter'>
+                <h4>© 2023 The Missing Piece Team</h4>
             </div>
         </>
     );
-};
+}
 
 function UserData({ icon, label, data }) {
     return (
