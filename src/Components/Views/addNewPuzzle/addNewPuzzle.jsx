00dfc30996e74ec21cb0da3bbf6ebe6f028ea5b9
Natalia392 <ntorrejonvalencia@gmail.com>
2023-10-22 15:56:35 -0300

    natalia392 feat: create function to upload image to cloudinary. Unify all inputs to add new puzzle in a form

--- src/Components/Views/addNewPuzzle/addNewPuzzle.jsx
+++ src/Components/Views/addNewPuzzle/addNewPuzzle.jsx
@@ -8,12 +8,31 @@ import NavigationBar from "../../commonComponents/navigationBar/navigationBar";
 export default function AddNewPuzzle() {
     const [image, setImage] = useState(null);
 
+    const handleImageUploadToCloudinary = (imageData) => {
+        const formData = new FormData();
+        formData.append('file', imageData); // Use 'file' as the key
+        formData.append('upload_preset', 'xk6d3n0j'); // Replace with your Cloudinary upload preset
+
+        fetch(`https://api.cloudinary.com/v1_1/dwcorjdyo/image/upload`, {
+            method: "POST", // Specify the method here
+            body: formData,
+        })
+            .then((response) => {
+                console.log(response);
+                return response.json();
+            })
+            .catch((error) => {
+                console.error('Error uploading to Cloudinary:', error);
+            });
+    };
+
     const handleImageUpload = (e) => {
         const file = e.target.files[0];
         if (file) {
             const reader = new FileReader();
             reader.onloadend = () => {
                 setImage(reader.result);
+                handleImageUploadToCloudinary(reader.result);
             };
             reader.readAsDataURL(file);
         }
@@ -33,38 +52,47 @@ export default function AddNewPuzzle() {
             <div className="container-new-puzzle">
                 <div className="left-side">
                     <div className="form-container">
-                        <h2 className='title'>Add a new puzzle</h2>
-                        <label className='subtitles'>Title</label>
-                        <input className='input' placeholder='Add a title for your puzzle' />
-                        <label className='subtitles'>Description</label>
-                        <textarea className='text-area' placeholder='Add a description' />
-                        <label className='subtitles'>Pieces Total Count</label>
-                        <input className='input' placeholder='Total pieces' />
-                        <label className='subtitles'>Notes</label>
-                        <textarea className='text-area' placeholder='Add any notes you want, for example, if the puzzle is in good condition or not, etc.' />
-                    </div>
-                </div>
-                <div className="right-side">
-                    <div className="upload-image">
-                        <img className='image' src={image || upload_image} alt="Upload" />
-                        {image && (
-                            <div className="icon-close" onClick={handleImageRemove}>
-                                <FaTimes size={20} />
-                            </div>
-                        )}
-                        <input type="file" onChange={handleImageUpload} style={{ display: 'none' }} id="image-upload" />
-                        <button onClick={() => document.getElementById('image-upload').click()} className="yellow-button">
-                            Upload Image
-                        </button>
-                    </div>
+                        <form className="add-pluzzle-form">
+                            <h2 className='title'>Add a new puzzle</h2>
 
-                    <Link to="/index">
-                        <button className="add-puzzle-button">Add New Puzzle</button>
-                    </Link>
-                    <Link to="/index">
-                        <button className="return-dashboard-button">Return to dashboard</button>
-                    </Link>
+                            <label className='subtitles'>Title</label>
+                            <input className='input' placeholder='Add a title for your puzzle' />
+
+                            <label className='subtitles'>Description</label>
+                            <textarea className='text-area' placeholder='Add a description' />
+
+                            <label className='subtitles'>Pieces Total Count</label>
+                            <input className='input' placeholder='Total pieces' />
+
+                            <label className='subtitles'>Notes</label>
+                            <textarea className='text-area' placeholder='Add any notes you want, for example, if the puzzle is in good condition or not, etc.' />
+
+                            <div className="right-side">
+                                <div className="upload-image">
+                                    <img className='image' src={image || upload_image} alt="Upload" />
+
+                                    {image && (
+                                        <div className="icon-close" onClick={handleImageRemove}>
+                                            <FaTimes size={20} />
+                                        </div>
+                                    )}
+
+                                    <input type="file" onChange={handleImageUpload} style={{ display: 'none' }} id="image-upload" />
+                                    <button type="button" onClick={() => document.getElementById('image-upload').click()} className="yellow-button">
+                                        Upload Image
+                                    </button>
+                                </div>
+
+                                <Link to="/index">
+                                    <button type="submit" className="add-puzzle-button">Add New Puzzle</button>
+                                </Link>
+                            </div>
+                        </form>
+                    </div>
                 </div>
+                <Link to="/index">
+                    <button className="return-dashboard-button">Return to dashboard</button>
+                </Link>
             </div>
             <div className='foo-ter'>
                 <h4>© 2023 The Missing Piece Team</h4>
